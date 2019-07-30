11076ccf2a9246542cbb93019a34b7175f80122b
Robert Jones <47233435+robertjonesdev@users.noreply.github.com>
2019-07-29 20:27:19 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*********************************************************************
 ** Author:        Robert Jones
 ** Date:          January 7, 2019
-** Description:   Main.cpp: The program implements Langston's Ant Simulation
+** Description:   Main.cpp: The program implements Langton's Ant Simulation
 **                that moves an ant around a board, by predefined rules,
 **                changing the board color from white to black or black to white.
 **                The proram begins (and loops) in a menu state that allows for
@@ -21,7 +21,7 @@ int main()
   std::string menuTitle = "    --- MENU ---";
 
   std::vector<std::string> menuList;
-  menuList.push_back("Start Langston's Ant simulation");
+  menuList.push_back("Start Langton's Ant simulation");
 
   Menu menu(menuTitle, menuList);
 
